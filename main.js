acf1c6f04819476f8707b90c6fe27bb06076a4e3
kwtietze <100501841+kwtietze@users.noreply.github.com>
2023-01-29 16:11:56 -0500

    completed

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
 function spotifyCalculate() {
     let rate = 0.0033
     let streams = document.getElementById('spotify-streams').value
+    let percentageField = document.getElementById('spotify-percentage')
     let percentage = document.getElementById('spotify-percentage').value
     let total = document.getElementById('spotify-total')
 
-    total.innerText = rate*streams*(percentage/100)
+    total.innerText = '$' + (rate*streams*(percentage/100)).toFixed(2)
 }
 
 
@@ -18,7 +19,7 @@ function appleCalculate() {
     let percentage = document.getElementById('apple-percentage').value
     let total = document.getElementById('apple-total')
 
-    total.innerText = rate*streams*(percentage/100)
+    total.innerText = '$' + (rate*streams*(percentage/100)).toFixed(2)
 }
 
 
@@ -30,17 +31,31 @@ function tidalCalculate() {
     let percentage = document.getElementById('tidal-percentage').value
     let total = document.getElementById('tidal-total')
 
-    total.innerText = rate*streams*(percentage/100)
+    total.innerText = '$' + (rate*streams*(percentage/100)).toFixed(2)
 }
+
+
 
 // nav selected styles
 
-const spotifyLink = document.querySelector('spotify-link')
-const appleLink = document.querySelector('apple-link')
-const tidalLink = document.querySelector('tidal-link')
+const spotifyLink = document.querySelector('.spotify-link')
+const appleLink = document.querySelector('.apple-link')
+const tidalLink = document.querySelector('.tidal-link')
 
 function tidalSelected() {
     spotifyLink.classList.remove('nav-selected')
     appleLink.classList.remove('nav-selected')
     tidalLink.classList.add('nav-selected')
+}
+
+function appleSelected() {
+    spotifyLink.classList.remove('nav-selected')
+    appleLink.classList.add('nav-selected')
+    tidalLink.classList.remove('nav-selected')
+}
+
+function spotifySelected() {
+    spotifyLink.classList.add('nav-selected')
+    appleLink.classList.remove('nav-selected')
+    tidalLink.classList.remove('nav-selected')
 }
